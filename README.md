657035e0fa47e47e93b675b8c71076fe8aca4204
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2021-12-23 14:00:52 +0300

    add phpmyadmin

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ alias docker-compose='/usr/local/bin/docker-compose'
 create Dockerfile and docker-compose.yaml
 
 docker-compose.yaml - container setup
+
+docker ps - see all running containers
+
+docker exec -it container_name sh
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('hello', function(){
-    return 'hello';
-});
+Route::get('hello', 'UserController@index');
